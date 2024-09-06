8ab303b1fb74a11b86149c9a52aa97ca7312912e
LostInDistantSpace <42539089+LostInDistantSpace@users.noreply.github.com>
2024-09-06 14:09:15 +0200

    Add files via upload

---
+++ client.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmouhib <bmouhib@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/27 18:54:21 by bmouhib           #+#    #+#             */
+/*   Updated: 2024/09/01 21:24:02 by bmouhib          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include "signal.h"
+
+static int	g_receiver;
+
+void	sig_handler(int signum, siginfo_t *info, void *context)
+{
+	static int	bytes = 0;
+
+	(void)info;
+	(void)context;
+	g_receiver = 1;
+	if (signum == SIGUSR2)
+		bytes++;
+	else if (signum == SIGUSR1)
+	{
+		ft_printf("Bytes received : %d\n", bytes / 8);
+		exit(0);
+	}
+}
+
+int	send_byte(int server_pid, char byte)
+{
+	int		shift;
+
+	shift = 7;
+	while (shift >= 0)
+	{
+		if ((byte >> shift) & 1)
+			kill(server_pid, SIGUSR1);
+		else
+			kill(server_pid, SIGUSR2);
+		while (!g_receiver)
+			;
+		g_receiver = 0;
+		--shift;
+	}
+	return (0);
+}
+
+int	send_string(int server_pid, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		send_byte(server_pid, str[i++]);
+	send_byte(server_pid, '\0');
+	return (0);
+}
+
+void	waiter(void)
+{
+	int	i;
+
+	i = 0;
+	while (i < 500)
+	{
+		if (i == 500)
+			exit (ft_printf("No server response...\n"));
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int					pid;
+	struct sigaction	sa;
+
+	if (ac > 3)
+		ft_printf("\nToo many arguments\n\n");
+	else if (ac < 3)
+		ft_printf("\nNo message to send\n\n");
+	if (ac != 3)
+		return (1);
+	pid = ft_atoi(av[1]);
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART | SA_SIGINFO;
+	sa.sa_sigaction = sig_handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	send_string(pid, av[2]);
+	waiter();
+	return (0);
+}
---
+++ server.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   server.c										   :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: bmouhib <bmouhib@student.42.fr>			+#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2024/07/27 18:53:33 by bmouhib		   #+#	#+#			 */
+/*   Updated: 2024/08/25 15:09:09 by bmouhib		  ###   ########.fr	   */
+/*																			*/
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <signal.h>
+
+void	ft_prompt(int serv_pid)
+{
+	static int	pid = 0;
+
+	if (!pid && serv_pid)
+		pid = serv_pid;
+	if (!serv_pid)
+		ft_printf("\n");
+	ft_printf("\033[0;37m\033[1m[PID: %d ]\033[0m ", pid);
+}
+
+void	bit_handler(int signal, char *byte)
+{
+	if (signal == SIGUSR1)
+		*byte = (*byte << 1) | 1;
+	else if (signal == SIGUSR2)
+		*byte <<= 1;
+}
+
+void	sig_handler(int signum, siginfo_t *info, void *context)
+{
+	static int	pid = 0;
+	static char	c = 0;
+	static int	i = 0;
+
+	(void)context;
+	if (pid == 0)
+		pid = info->si_pid;
+	bit_handler(signum, &c);
+	if (++i == 8)
+	{
+		i = 0;
+		if (!c)
+		{
+			ft_prompt(0);
+			kill(pid, SIGUSR1);
+			pid = 0;
+			return ;
+		}
+		ft_printf("%c", c);
+		c = 0;
+	}
+	kill(pid, SIGUSR2);
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
+
+	ft_prompt(getpid());
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART | SA_SIGINFO;
+	sa.sa_sigaction = sig_handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
+	return (0);
+}
